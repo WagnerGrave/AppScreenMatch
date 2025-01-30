09ad298b07d09d57d1221645191528419cfa7023
Wagner Grave <130260641+WagnerGrave@users.noreply.github.com>
2025-01-29 22:39:29 -0300

    Update PrincipalComBusca.java

--- src/wagner/dev/appscreenmatch/principal/PrincipalComBusca.java
+++ src/wagner/dev/appscreenmatch/principal/PrincipalComBusca.java
@@ -50,7 +50,7 @@ public class PrincipalComBusca {
 
                 TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
                 System.out.println(meuTituloOmdb);
-                //  try {
+                
                 Titulo meuTitulo = new Titulo(meuTituloOmdb);
                 System.out.println("Titulo já convertido");
                 System.out.println(meuTitulo);
